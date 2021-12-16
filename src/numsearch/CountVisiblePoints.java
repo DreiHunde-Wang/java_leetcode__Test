03ed4dec377cb0ca7f0f19c8dfdd483bc70240cf
DreiHunde-Wang <52560784+DreiHunde-Wang@users.noreply.github.com>
2021-12-16 20:52:06 +0800

    add

---
+++ src/numsearch/CountVisiblePoints.java
@@ -0,0 +1,97 @@
+package numsearch;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CountVisiblePoints {
+	//method 1 二分查找 O(nlogn) O(n)
+    public int visiblePoints1(List<List<Integer>> points, int angle, List<Integer> location) {
+        int sameCnt = 0;
+        List<Double> polarDegrees = new ArrayList<>();
+        int locationX = location.get(0);
+        int locationY = location.get(1);
+        for (int i = 0; i < points.size(); ++i) {
+            int x = points.get(i).get(0);
+            int y = points.get(i).get(1);
+            if (x == locationX && y == locationY) {
+                sameCnt++;
+                continue;
+            }
+            Double degree = Math.atan2(y - locationY, x - locationX);
+            polarDegrees.add(degree);
+        }
+        Collections.sort(polarDegrees);
+
+        int m = polarDegrees.size();
+        for (int i = 0; i < m; ++i) {
+            polarDegrees.add(polarDegrees.get(i) + 2 * Math.PI);
+        }
+
+        int maxCnt = 0;
+        Double toDegree = angle * Math.PI / 180;
+        for (int i = 0; i < m; ++i) {
+            int iteration = binarySearch(polarDegrees, polarDegrees.get(i) + toDegree, false);
+            maxCnt = Math.max(maxCnt, iteration - i);
+        }
+        return maxCnt + sameCnt;
+    }
+
+    public int binarySearch(List<Double> nums, Double target, boolean lower) {
+        int left = 0, right = nums.size() - 1;
+        int ans = nums.size();
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums.get(mid) > target || (lower && nums.get(mid) >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+
+    //method 2 滑动窗口 O(nlogn) O(n)
+    public int visiblePoints(List<List<Integer>> points, int angle, List<Integer> location) {
+        int sameCnt = 0;
+        List<Double> polarDegrees = new ArrayList<>();
+        int locationX = location.get(0);
+        int locationY = location.get(1);
+        for (int i = 0; i < points.size(); ++i) {
+            int x = points.get(i).get(0);
+            int y = points.get(i).get(1);
+            if (x == locationX && y == locationY) {
+                sameCnt++;
+                continue;
+            }
+            Double degree = Math.atan2(y - locationY, x - locationX);
+            polarDegrees.add(degree);
+        }
+        Collections.sort(polarDegrees);
+
+        int m = polarDegrees.size();
+        for (int i = 0; i < m; ++i) {
+            polarDegrees.add(polarDegrees.get(i) + 2 * Math.PI);
+        }
+
+        int maxCnt = 0;
+        int right = 0;
+        double toDegree = angle * Math.PI / 180; 
+        for (int i = 0; i < m; ++i) {
+            Double curr = polarDegrees.get(i) + toDegree;
+            while (right < polarDegrees.size() && polarDegrees.get(right) <= curr) {
+                right++;
+            }
+            maxCnt = Math.max(maxCnt, right - i);
+        }
+        return maxCnt + sameCnt;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
