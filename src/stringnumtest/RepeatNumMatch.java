bfb800509e8c1f9b45cc9b94124e6eed9d90727d
DreiHunde-Wang <52560784+DreiHunde-Wang@users.noreply.github.com>
2021-12-22 23:07:14 +0800

    add

---
+++ src/stringnumtest/RepeatNumMatch.java
@@ -0,0 +1,120 @@
+package stringnumtest;
+
+import java.util.Random;
+
+/**
+ * 给定两个字符串 a 和 b，寻找重复叠加字符串 a 的最小次数，使得字符串 b 成为叠加后的字符串 a 的子串，如果不存在则返回 -1。
+ * 注意：字符串 "abc" 重复叠加 0 次是 ""，重复叠加 1 次是 "abc"，重复叠加 2 次是 "abcabc"。
+ * @author Dreihunde
+ *
+ */
+public class RepeatNumMatch {
+	//method 1 Rabin-Karp 算法 O(m + n) O(1)
+    static final int kMod1 = 1000000007;
+    static final int kMod2 = 1337;
+
+    public int repeatedStringMatch(String a, String b) {
+        int an = a.length(), bn = b.length();
+        int index = strStr(a, b);
+        if (index == -1) {
+            return -1;
+        }
+        if (an - index >= bn) {
+            return 1;
+        }
+        return (bn + index - an - 1) / an + 2;
+    }
+
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        if (m == 0) {
+            return 0;
+        }
+
+        int k1 = 1000000009;
+        int k2 = 1337;
+        Random random = new Random();
+        int kMod1 = random.nextInt(k1) + k1;
+        int kMod2 = random.nextInt(k2) + k2;
+
+        long hashNeedle = 0;
+        for (int i = 0; i < m; i++) {
+            char c = needle.charAt(i);
+            hashNeedle = (hashNeedle * kMod2 + c) % kMod1;
+        }
+        long hashHaystack = 0, extra = 1;
+        for (int i = 0; i < m - 1; i++) {
+            hashHaystack = (hashHaystack * kMod2 + haystack.charAt(i % n)) % kMod1;
+            extra = (extra * kMod2) % kMod1;
+        }
+        for (int i = m - 1; (i - m + 1) < n; i++) {
+            hashHaystack = (hashHaystack * kMod2 + haystack.charAt(i % n)) % kMod1;
+            if (hashHaystack == hashNeedle) {
+                return i - m + 1;
+            }
+            hashHaystack = (hashHaystack - extra * haystack.charAt((i - m + 1) % n)) % kMod1;
+            hashHaystack = (hashHaystack + kMod1) % kMod1;
+        }
+        return -1;
+    }
+    //method 2 KMP算法 O(m + n) O(m)
+    public int repeatedStringMatch2(String a, String b) {
+        int an = a.length(), bn = b.length();
+        int index = strStr(a, b);
+        if (index == -1) {
+            return -1;
+        }
+        if (an - index >= bn) {
+            return 1;
+        }
+        return (bn + index - an - 1) / an + 2;
+    }
+
+    public int strStr2(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        if (m == 0) {
+            return 0;
+        }
+        int[] pi = new int[m];
+        for (int i = 1, j = 0; i < m; i++) {
+            while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (needle.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            pi[i] = j;
+        }
+        for (int i = 0, j = 0; i - j < n; i++) { // b 开始匹配的位置是否超过第一个叠加的 a
+            while (j > 0 && haystack.charAt(i % n) != needle.charAt(j)) { // haystack 是循环叠加的字符串，所以取 i % n
+                j = pi[j - 1];
+            }
+            if (haystack.charAt(i % n) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == m) {
+                return i - m + 1;
+            }
+        }
+        return -1;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "abcd";
+		String b = "cdabcdab";
+		
+		RepeatNumMatch rn = new RepeatNumMatch();
+		long startTime=System.nanoTime(); 
+		System.out.println(rn.repeatedStringMatch(a, b));
+		long endTime=System.nanoTime(); 
+		System.out.println("time cost:" + (endTime - startTime)/1000 + "ms");
+		
+		startTime=System.nanoTime(); 
+		System.out.println(rn.repeatedStringMatch2(a, b));
+		endTime=System.nanoTime(); 
+		System.out.println("time cost:" + (endTime - startTime)/1000 + "ms");
+
+	}
+
+}
