08e64a70a6e79c9fbf4f0f4d3ce39c27d65893f5
DreiHunde-Wang <52560784+DreiHunde-Wang@users.noreply.github.com>
2022-01-21 23:10:01 +0800

    add

---
+++ src/dfsandbfs/MinJump.java
@@ -0,0 +1,66 @@
+package dfsandbfs;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * 给你一个整数数组 arr ，你一开始在数组的第一个元素处（下标为 0）。
+ * 每一步，你可以从下标 i 跳到下标：
+ * i + 1 满足：i + 1 < arr.length
+ * i - 1 满足：i - 1 >= 0
+ * j 满足：arr[i] == arr[j] 且 i != j
+ * 请你返回到达数组最后一个元素的下标处所需的 最少操作次数 。
+ * 注意：任何时候你都不能跳到数组外面。
+ * @author Dreihunde
+ *
+ */
+public class MinJump {
+	//BFS + 记忆化 O(n) O(n)
+    public int minJumps(int[] arr) {
+        Map<Integer, List<Integer>> idxSameValue = new HashMap<Integer, List<Integer>>();
+        for (int i = 0; i < arr.length; i++) {
+            idxSameValue.putIfAbsent(arr[i], new ArrayList<Integer>());
+            idxSameValue.get(arr[i]).add(i);
+        }
+        Set<Integer> visitedIndex = new HashSet<Integer>();
+        Queue<int[]> queue = new ArrayDeque<int[]>();
+        queue.offer(new int[]{0, 0});
+        visitedIndex.add(0);
+        while (!queue.isEmpty()) {
+            int[] idxStep = queue.poll();
+            int idx = idxStep[0], step = idxStep[1];
+            if (idx == arr.length - 1) {
+                return step;
+            }
+            int v = arr[idx];
+            step++;
+            if (idxSameValue.containsKey(v)) {
+                for (int i : idxSameValue.get(v)) {
+                    if (visitedIndex.add(i)) {
+                        queue.offer(new int[]{i, step});
+                    }
+                }
+                idxSameValue.remove(v);
+            }
+            if (idx + 1 < arr.length && visitedIndex.add(idx + 1)) {
+                queue.offer(new int[]{idx + 1, step});
+            }
+            if (idx - 1 >= 0 && visitedIndex.add(idx - 1)) {
+                queue.offer(new int[]{idx - 1, step});
+            }
+        }
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+		int[] arr = new int[] {100,-23,-23,404,100,23,23,23,3,404};
+		MinJump mj = new MinJump();
+		System.out.println(mj.minJumps(arr));
+	}
+}
