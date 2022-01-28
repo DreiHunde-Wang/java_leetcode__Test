7c9b43bcf77b058bdb8382d636d74b1d98a5aabc
DreiHunde-Wang <52560784+DreiHunde-Wang@users.noreply.github.com>
2022-01-29 00:02:13 +0800

    add

--- src/numsorttest/WeakHero.java
+++ src/numsorttest/WeakHero.java
@@ -4,6 +4,14 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
 
+/**
+ * 你正在参加一个多角色游戏，每个角色都有两个主要属性：攻击 和 防御 。给你一个二维整数数组 properties ，其中 properties[i] = [attacki, defensei] 表示游戏中第 i 个角色的属性。
+ * 如果存在一个其他角色的攻击和防御等级 都严格高于 该角色的攻击和防御等级，则认为该角色为 弱角色 。
+ * 更正式地，如果认为角色 i 弱于 存在的另一个角色 j ，那么 attackj > attacki 且 defensej > defensei 。
+ * https://leetcode-cn.com/problems/the-number-of-weak-characters-in-the-game/
+ * @author Dreihunde
+ *
+ */
 public class WeakHero {
 	//method 先排序 O(nlogn) O(logn)
     public int numberOfWeakCharacters1(int[][] properties) {
