f79bf55451c87f3c82cdfddef91b0655ca153a53
DreiHunde-Wang <52560784+DreiHunde-Wang@users.noreply.github.com>
2022-05-02 10:09:57 +0800

    add

---
+++ src/stringnumtest/IsVaildStr.java
@@ -0,0 +1,97 @@
+package stringnumtest;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 给定一个表示代码片段的字符串，你需要实现一个验证器来解析这段代码，并返回它是否合法。合法的代码片段需要遵守以下的所有规则：
+ * 代码必须被合法的闭合标签包围。否则，代码是无效的。
+ * 闭合标签（不一定合法）要严格符合格式：<TAG_NAME>TAG_CONTENT</TAG_NAME>。其中，<TAG_NAME>是起始标签，</TAG_NAME>是结束标签。
+ * 起始和结束标签中的 TAG_NAME 应当相同。当且仅当 TAG_NAME 和 TAG_CONTENT 都是合法的，闭合标签才是合法的。
+ * 合法的 TAG_NAME 仅含有大写字母，长度在范围 [1,9] 之间。否则，该 TAG_NAME 是不合法的。
+ * 合法的 TAG_CONTENT 可以包含其他合法的闭合标签，cdata （请参考规则7）和任意字符（注意参考规则1）除了不匹配的<、不匹配的起始和结束标签、不匹配的或带有不合法 TAG_NAME 的闭合标签。否则，TAG_CONTENT 是不合法的。
+ * 一个起始标签，如果没有具有相同 TAG_NAME 的结束标签与之匹配，是不合法的。反之亦然。不过，你也需要考虑标签嵌套的问题。
+ * 一个<，如果你找不到一个后续的>与之匹配，是不合法的。并且当你找到一个<或</时，所有直到下一个>的前的字符，都应当被解析为 TAG_NAME（不一定合法）。
+ * cdata 有如下格式：<![CDATA[CDATA_CONTENT]]>。CDATA_CONTENT 的范围被定义成 <![CDATA[ 和后续的第一个 ]]>之间的字符。
+ * CDATA_CONTENT 可以包含任意字符。cdata 的功能是阻止验证器解析CDATA_CONTENT，所以即使其中有一些字符可以被解析为标签（无论合法还是不合法），也应该将它们视为常规字符。
+ * 链接：https://leetcode-cn.com/problems/tag-validator
+ * @author Dreihunde
+ *
+ */
+public class IsVaildStr {
+	//method 1 遍历+stack O(n) O(n)
+    public boolean isValid(String code) {
+        int n = code.length();
+        Deque<String> tags = new ArrayDeque<String>();
+
+        int i = 0;
+        while (i < n) {
+            if (code.charAt(i) == '<') {
+                if (i == n - 1) {
+                    return false;
+                }
+                //当找到收尾tag时
+                if (code.charAt(i + 1) == '/') {
+                    int j = code.indexOf('>', i);
+                    if (j < 0) {
+                        return false;
+                    }
+                    //提取结尾tag
+                    String tagname = code.substring(i + 2, j);
+                    //与栈顶的tag进行匹配
+                    if (tags.isEmpty() || !tags.peek().equals(tagname)) {
+                        return false;
+                    }
+                    tags.pop();
+                    i = j + 1;
+                    //如果还没到结尾就把开头的tag匹配完了，就为false
+                    if (tags.isEmpty() && i != n) {
+                        return false;
+                    }
+                //当找到内容context时
+                } else if (code.charAt(i + 1) == '!') {
+                    if (tags.isEmpty()) {
+                        return false;
+                    }
+                    if (i + 9 > n) {
+                        return false;
+                    }
+                    String cdata = code.substring(i + 2, i + 9);
+                    if (!"[CDATA[".equals(cdata)) {
+                        return false;
+                    }
+                    int j = code.indexOf("]]>", i);
+                    if (j < 0) {
+                        return false;
+                    }
+                    i = j + 1;
+                //开头 tag
+                } else {
+                    int j = code.indexOf('>', i);
+                    if (j < 0) {
+                        return false;
+                    }
+                    String tagname = code.substring(i + 1, j);
+                    if (tagname.length() < 1 || tagname.length() > 9) {
+                        return false;
+                    }
+                    for (int k = 0; k < tagname.length(); ++k) {
+                        if (!Character.isUpperCase(tagname.charAt(k))) {
+                            return false;
+                        }
+                    }
+                    tags.push(tagname);
+                    i = j + 1;
+                }
+            } else {
+                if (tags.isEmpty()) {
+                    return false;
+                }
+                ++i;
+            }
+        }
+
+        return tags.isEmpty();
+    }
+
+}
