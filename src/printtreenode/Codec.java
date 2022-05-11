a91b2007c0aab8b1d02204a7c5757225480e0273
DreiHunde-Wang <52560784+DreiHunde-Wang@users.noreply.github.com>
2022-05-11 21:27:52 +0800

    add

--- src/printtreenode/Codec.java
+++ src/printtreenode/Codec.java
@@ -7,16 +7,17 @@ import java.util.List;
 import java.util.Queue;
 
 /**
- * 请实现两个函数，分别用来序列化和反序列化二叉树�??
- * 你需要设计一个算法来实现二叉树的序列化与反序列化�?
- * 这里不限定你的序�? / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构�??
- * 提示：输入输出格式与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式�??
- * 你并非必须采取这种方式，你也可以采用其他的方法解决这个问题�??
+ * 请实现两个函数，分别用来序列化和反序列化二叉树。
+ * 你需要设计一个算法来实现二叉树的序列化与反序列化。
+ * 这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+ * 提示：输入输出格式与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。
+ * 你并非必须采取这种方式，你也可以采用其他的方法解决这个问题。
+ * 链接：https://leetcode.cn/problems/xu-lie-hua-er-cha-shu-lcof
  * @author Dreihunde
  *
  */
 public class Codec {
-	//method 1 先序遍历（�?�归）O(n) O(n)
+	//method 1 先序遍历（递归）O(n) O(n)
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
     	String str = reserialize(root, "");
@@ -97,7 +98,7 @@ class Codec2 {
             	queue.add(temp.right);
         	}
         }
-        //删除�?后一个�?�号
+        //删除最后一个，号
         sb.deleteCharAt(sb.length() - 1);
         return sb.toString();
     }
