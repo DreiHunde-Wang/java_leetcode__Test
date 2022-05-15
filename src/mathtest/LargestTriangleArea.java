3084fbe7d652882b15be8c40cb5a494133958598
DreiHunde-Wang <52560784+DreiHunde-Wang@users.noreply.github.com>
2022-05-15 20:52:08 +0800

    add

---
+++ src/mathtest/LargestTriangleArea.java
@@ -0,0 +1,98 @@
+package src.mathtest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LargestTriangleArea {
+	//method 1 模拟 O(n^3) O(1)
+    public double largestTriangleArea1(int[][] points) {
+        double ans = 0;
+        int n = points.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    ans = Math.max(ans, getArea(points[i], points[j], points[k]));
+                }
+            }
+        }
+        return ans;
+    }
+    //        |x1 y1 1|
+    // area = |x2 y2 1|
+    //        |x3 y3 1|
+    public double getArea(int[] p1, int[] p2, int[] p3) {
+        return 0.5 * Math.abs(p1[0] * p2[1] +  p1[1] * p3[0] + p2[0] * p3[1] - p2[1] * p3[0] - p3[1] * p1[0] - p1[1] * p2[0]);
+    }
+    
+    
+  //method 2 凸包算法 O(n^2) O(n)
+    public double largestTriangleArea(int[][] points) {
+        //先使用Andrew算法求出所有点对应的凸包convexHull
+        int[][] convexHull = getConvexHull(points);
+        int n = convexHull.length;
+        double ret = 0.0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1, k = i + 2; j + 1 < n; j++) {
+                while (k + 1 < n) {
+                    double curArea = triangleArea(convexHull[i][0], convexHull[i][1], convexHull[j][0], convexHull[j][1], convexHull[k][0], convexHull[k][1]);
+                    double nextArea = triangleArea(convexHull[i][0], convexHull[i][1], convexHull[j][0], convexHull[j][1], convexHull[k + 1][0], convexHull[k + 1][1]);
+                    if (curArea >= nextArea) {
+                        break;
+                    }
+                    k++;
+                }
+                double area = triangleArea(convexHull[i][0], convexHull[i][1], convexHull[j][0], convexHull[j][1], convexHull[k][0], convexHull[k][1]);
+                ret = Math.max(ret, area);
+            }
+        }
+        return ret;
+    }
+
+    public int[][] getConvexHull(int[][] points) {
+        int n = points.length;
+        if (n < 4) {
+            return points;
+        }
+        /* 按照 x 大小进行排序，如果 x 相同，则按照 y 的大小进行排序 */
+        Arrays.sort(points, (a, b) -> {
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
+            }
+            return a[0] - b[0];
+        });
+        List<int[]> hull = new ArrayList<int[]>();
+        /* 求出凸包的下半部分 */
+        for (int i = 0; i < n; i++) {
+            while (hull.size() > 1 && cross(hull.get(hull.size() - 2), hull.get(hull.size() - 1), points[i]) <= 0) {
+                hull.remove(hull.size() - 1);
+            }
+            hull.add(points[i]);
+        }
+        int m = hull.size();
+        /* 求出凸包的上半部分 */
+        for (int i = n - 2; i >= 0; i--) {
+            while (hull.size() > m && cross(hull.get(hull.size() - 2), hull.get(hull.size() - 1), points[i]) <= 0) {
+                hull.remove(hull.size() - 1);
+            }
+            hull.add(points[i]);
+        }
+        /* hull[0] 同时参与凸包的上半部分检测，因此需去掉重复的 hull[0] */
+        hull.remove(hull.size() - 1);
+        m = hull.size();
+        int[][] hullArr = new int[m][];
+        for (int i = 0; i < m; i++) {
+            hullArr[i] = hull.get(i);
+        }
+        return hullArr;
+    }
+
+    public double triangleArea(int x1, int y1, int x2, int y2, int x3, int y3) {
+        return 0.5 * Math.abs(x1 * y2 + x2 * y3 + x3 * y1 - x1 * y3 - x2 * y1 - x3 * y2);
+    }
+
+    public int cross(int[] p, int[] q, int[] r) {
+        return (q[0] - p[0]) * (r[1] - q[1]) - (q[1] - p[1]) * (r[0] - q[0]);
+    }
+
+}
